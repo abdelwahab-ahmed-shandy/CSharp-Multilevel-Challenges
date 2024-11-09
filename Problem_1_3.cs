e7fb4fbb8a79c565871f6c76b1adbbe16743edfe
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-11-09 19:09:54 +0200

    Update Problem_1_3.cs

--- Problem_1_3.cs
+++ Problem_1_3.cs
@@ -1,22 +1,26 @@
 //Problem_3 >> Write a program to ask the user to enter a number , then print "ODD" if its odd , or "EVEN if its even
 
-using System.Threading.Channels;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace _1to_6
+namespace Contrling_Flow
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-               
             Console.Write("Enter Your Number : ");
-            int Number = Console.Read();
-           
-            if (Number % 2 == 0)
-                Console.WriteLine("This Number is EVEN");
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number % 2 == 1)
+            {
+                Console.WriteLine("ODD");
+            }
+            else if (Number == 0 )
+            {
+                Console.WriteLine("Zero is Neither ODD or EVEN.");
+            }
             else
-                Console.WriteLine("This Number is ODD");
+            {
+                Console.WriteLine("EVEN");
+            }
         }
     }
 }
