7c34d06d32abb4b20d998fe0f7168cec1497e803
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-11-09 22:31:00 +0200

    Update Problem_1_7.cs

--- Problem_1_7.cs
+++ Problem_1_7.cs
@@ -1,5 +1,5 @@
 /*
-1. Write a C# Sharp program to print Hello and your name in a separate line.
+Write a C# Sharp program to print Hello and your name in a separate line.
 Expected Output :
 Hello: Alexandra Abramov
 */
