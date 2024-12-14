4ed2ed57bc6744948210f6c6d0e6c1977212745f
Abdelwahab Ahmed Shandy <119771784+Abdelwahab-Shandy@users.noreply.github.com>
2024-12-14 22:41:12 +0200

    Update P_2_6_CalculateRectangleArea.cs

--- Solve Fifty Problems With Flowcharts (Beginner - Intermediate)/Beginner/P_2_6_CalculateRectangleArea.cs
+++ Solve Fifty Problems With Flowcharts (Beginner - Intermediate)/Beginner/P_2_6_CalculateRectangleArea.cs
@@ -14,9 +14,9 @@ namespace PracticalExample
         public static void ReadNumbers(ref double Length, ref double Width)
         {
             Console.WriteLine("Rectangle Area Write a program to calculate rectangle area and print it on the screen  , Area = a*b\n");
-            Console.Write("Enter Number One  : ");
+            Console.Write("Enter Length  : ");
             Length = double.Parse(Console.ReadLine());
-            Console.Write("Enter Number Two  : ");
+            Console.Write("Enter Width  : ");
             Width = double.Parse(Console.ReadLine());   
         }
         public static double CalcRectangleArea(double Length, double Width)
